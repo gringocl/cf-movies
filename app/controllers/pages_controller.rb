13fb79383adbb74807235bbd237eb671567f41c0
Miles Starkenburg <milesstarkenburg@gmail.com>
2013-09-01 18:53:16 -0700

    Add instance variables to home action, migrate db, and seed with data.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,11 @@
 class PagesController < ApplicationController
 
   def home
+  	@first = Movie.first.title
+    @first_votes = Movie.first.votes.count
   end
 
+  def index
+  	@movies = Movie.all
+  end
 end
