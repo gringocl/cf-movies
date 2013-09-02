8934518b091b77ea9bc641e63cf4f8b04630839e
Miles Starkenburg <milesstarkenburg@gmail.com>
2013-09-01 20:34:45 -0700

    Add my favorite movie to db and display it on the home page.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,7 @@ class PagesController < ApplicationController
   def home
   	@first = Movie.first.title
     @first_votes = Movie.first.votes.count
+    @my_favorite = Movie.find_by_title("Diarios de motocicleta")
   end
 
   def index
